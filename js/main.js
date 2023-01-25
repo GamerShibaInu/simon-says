19831a002827d5985cb650ddf30b7f17c674fb13
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-25 10:29:32 +0100

    added color generator, plr input and to next lvl

--- js/main.js
+++ js/main.js
@@ -12,16 +12,41 @@ const green = document.getElementsByClassName("green");
 const blue = document.getElementsByClassName("blue");
 
 // adds event listeners to color divs
-red.addEventListener("click", function() {
-  playerInputColor("red");
+red.addEventListener("click", function () {
+    playerInputColor("red");
 });
-yellow.addEventListener("click", function() {
-  playerInputColor("yellow");
+yellow.addEventListener("click", function () {
+    playerInputColor("yellow");
 });
-green.addEventListener("click", function() {
-  playerInputColor("green");
+green.addEventListener("click", function () {
+    playerInputColor("green");
 });
-blue.addEventListener("click", function() {
-  playerInputColor("blue");
+blue.addEventListener("click", function () {
+    playerInputColor("blue");
 });
 
+// player input color function
+function playerInputColor(color) {
+    plrColor.push(color);
+    checkPlayerInput();
+}
+
+// random color generator for array
+function generateBotColor() {
+    const colors = ["red", "green", "blue", "yellow"];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    botColor.push(randomColor);
+  }
+
+
+  // next level
+  function nextLevel() {
+    generateBotColor();
+    for (let i = 0; i < botColor.length; i++) {
+      document.getElementsByClassName(botColor[i]).style.backgroundColor = "white";
+      setTimeout(function() {
+        document.getElementsByClassName(botColor[i]).style.backgroundColor = botColor[i];
+      }, 1000);
+    }
+  }
