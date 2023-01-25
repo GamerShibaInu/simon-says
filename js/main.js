16e5fe485b43bd6642c4516dba965be39edb56d6
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-25 10:56:38 +0100

    added plr input and checks

--- js/main.js
+++ js/main.js
@@ -14,3 +14,33 @@ playerInputColor(color);
 });
 });
 
+// Player input color function
+function playerInputColor(color) {
+    plrColor.push(color);
+    checkPlayerInput();
+    }
+    
+// Compare the botColor and plrColor arrays
+function checkPlayerInput() {
+    for (let i = 0; i < botColor.length; i++) {
+        if (botColor[i] !== plrColor[i]) {
+            if(plrColor.length == botColor.length) {
+                console.log("Incorrect input, game over!");
+                location.reload(); 
+                return;
+            }
+            else {
+                return;
+            }
+        }
+    }
+    if (plrColor.length === botColor.length) {
+        score++;
+        level++;
+        console.log("Correct input, score: " + score + ", level: " + level);
+        plrColor = [];
+        document.querySelector(".score").innerHTML = "score: " + score;
+        setTimeout(nextLevel, 1000);
+    }
+}
+
