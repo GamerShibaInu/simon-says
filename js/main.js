d2542ebce01124ef445ba20d522539aa24b91cf5
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-25 10:26:29 +0100

    added event listeners and get elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+//variables
 let score = 0
+let level = 0;
+//array's
 let botColor = [];
 let plrColor = [];
+
+// gets color divs
+const red = document.getElementsByClassName("red");
+const yellow = document.getElementsByClassName("yellow");
+const green = document.getElementsByClassName("green");
+const blue = document.getElementsByClassName("blue");
+
+// adds event listeners to color divs
+red.addEventListener("click", function() {
+  playerInputColor("red");
+});
+yellow.addEventListener("click", function() {
+  playerInputColor("yellow");
+});
+green.addEventListener("click", function() {
+  playerInputColor("green");
+});
+blue.addEventListener("click", function() {
+  playerInputColor("blue");
+});
+
