2b01c151a3cfacb55439043663e1a13a17101222
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-26 12:48:44 +0100

    added high score

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 //variables
-let score = 0
+let score = 0;
 let level = 0;
+let highScorePoints = 0;
 //array's
 let botColor = [];
 let plrColor = [];
-
+//load highscore
+const vHighScore = localStorage.getItem("highScore");
+const highScoreText = document.querySelector(".high-score")
+highScoreText.textContent = "high score: " + vHighScore
 // add event listeners to color divs
 const colorDivs = document.querySelectorAll(".color");
 colorDivs.forEach(function (colorDiv) {
@@ -25,6 +29,7 @@ function checkPlayerInput() {
     for (let i = 0; i < botColor.length; i++) {
         if (botColor[i] !== plrColor[i]) {
             if (plrColor.length == botColor.length) {
+                highScorePoints = 0 + score
                 console.log("Incorrect input, game over!");
                 location.reload();
                 return;
@@ -42,8 +47,11 @@ function checkPlayerInput() {
         document.querySelector(".score").innerHTML = "score: " + score;
         setTimeout(nextLevel, 1000);
     }
-}
-
+    if (score >= highScorePoints) {
+        highScorePoints = 0 + score
+        localStorage.setItem("highScore", score);
+    }
+}   
 // next level function
 function nextLevel() {
     // add a random color to the botColor array
